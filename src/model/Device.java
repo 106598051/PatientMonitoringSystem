f0af2bdeb973aac03421d6e2ebf17ad87dbcf296
JEFFREY-ISLAB\Jeffrey <jeffr500000@gmail.com>
2018-01-15 14:18:43 +0800

    Construct basic parameters.

--- src/model/Device.java
+++ src/model/Device.java
@@ -1,5 +1,58 @@
 package model;
 
-public class Device {
-
+/**
+ * The Class Device.
+ * Contain device category, name, safe range.
+ */
+class Device {
+  
+  /** The category. */
+  private String category;
+  
+  /** The name. */
+  private String name;
+  
+  /** The safe range. */
+  private SafeRange safeRange;
+  
+  /**
+   * Instantiates a new device.
+   *
+   * @param category the category
+   * @param name the name
+   * @param safeRangeLowerBound the safe range lower bound
+   * @param safeRangeUpperBound the safe range upper bound
+   */
+  public Device(String category, String name, int safeRangeLowerBound, int safeRangeUpperBound) {
+    this.category = category;
+    this.name = name;
+    safeRange = new SafeRange(safeRangeLowerBound, safeRangeUpperBound);
+  }
+  
+  /**
+   * Category accessor methods.
+   *
+   * @return the string
+   */
+  public String category() {
+    return this.category;
+  }
+  
+  /**
+   * Name accessor methods.
+   *
+   * @return the string
+   */
+  public String name() {
+    return this.name;
+  }
+  
+  /**
+   * Safe range accessor methods.
+   *
+   * @return the safe range
+   */
+  public SafeRange safeRange() {
+    return this.safeRange;
+  }
 }
--- src/model/SafeRange.java
+++ src/model/SafeRange.java
@@ -1,5 +1,46 @@
 package model;
 
+/**
+ * The Class SafeRange.
+ * Contain safe range of device.
+ */
 class SafeRange {
-
+  
+  /** The lower bound. */
+  private int lowerBound;
+  
+  /** The upper bound. */
+  private int upperBound;
+  
+  /**
+   * Instantiates a new safe range.
+   *
+   * @param lowerBound the lower bound
+   * @param upperBound the upper bound
+   */
+  public SafeRange(int lowerBound, int upperBound) {
+    this.lowerBound = lowerBound;
+    this.upperBound = upperBound;
+  }
+  
+  /**
+   * Lower bound accessor methods.
+   *
+   * @return the int
+   */
+  public int lowerBound() {
+    return this.lowerBound;
+  }
+  
+  /**
+   * Upper bound accessor methods.
+   *
+   * @return the int
+   */
+  public int upperBound() {
+    return this.upperBound;
+  }
+  
+  
+  
 }
