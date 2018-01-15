f3d7ee190a0683702e91fcae965dfa95c7ddd05e
JEFFREY-ISLAB\Jeffrey <jeffr500000@gmail.com>
2018-01-15 13:48:35 +0800

    Initial.

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+/**
+ * @author Jeffrey
+ *
+ */
+public class Main {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    if (args.length > 0) {
+      System.out.println("args: " + args[0]);
+    } else {
+      System.out.println("No args.");
+    }
+  }
+
+}
