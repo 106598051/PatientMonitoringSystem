81c44adc4b53102ee03c7557b42014f0cf000930
JEFFREY-ISLAB\Jeffrey <jeffr500000@gmail.com>
2018-01-15 14:53:08 +0800

    Construct Patient basic parameter. Add org.json json library.

--- src/model/Patient.java
+++ src/model/Patient.java
@@ -1,5 +1,43 @@
 package model;
 
+/**
+ * The Class Patient.
+ * Contain patient name and sensor device period
+ */
 class Patient {
-
+  
+  /** The name. */
+  private String name;
+  
+  /** The period of sensor device. */
+  private int period;
+  
+  /**
+   * Instantiates a new patient.
+   *
+   * @param name the name
+   * @param period the period
+   */
+  public Patient(String name, int period) {
+    this.name = name;
+    this. period = period;
+  }
+  
+  /**
+   * Name of patient.
+   *
+   * @return the string
+   */
+  public String name() {
+    return this.name;    
+  }
+  
+  /**
+   * Period for sensor device of patient.
+   *
+   * @return the int
+   */
+  public int period() {
+    return this.period;
+  }
 }
