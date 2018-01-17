81524229e06be6a0a8e0cb4b4a5543b43adaad02
Jeffrey@ISLab <jeffr500000@gmail.com>
2018-01-17 17:53:36 +0800

    Add variable of factorRecord.

--- src/model/Monitor.java
+++ src/model/Monitor.java
@@ -16,6 +16,8 @@ public class Monitor {
 
   /** The timestamp. */
   private int timestamp;
+  
+  private ArrayList<String> factorRecord;
 
   /**
    * Instantiates a new monitor.
@@ -26,6 +28,7 @@ public class Monitor {
     this.monitorPeriod = monitorPeriod;
     this.patient = new ArrayList<Patient>();
     this.timestamp = 0;
+    this.factorRecord = new ArrayList<String>();
   }
 
   /**
